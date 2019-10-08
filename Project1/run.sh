f5639cd9fc18c256eede7dcfc56533bea99ca69d
Nicolai Haug <nicoha@student.matnat.uio.no>
2019-10-08 15:52:11 +0200

    Update run instructions

--- Project1/run.sh
+++ Project1/run.sh
@@ -1,3 +1,11 @@
-# Run and reproduce all results
+# Run tests and reproduce all results
 
 pytest -p no:warnings
+
+python3 analysis_ols.py
+
+python3 analysis_ridge.py
+
+python3 analysis_lasso.py
+
+python3 analysis_terrain.py
